509a9f38c07a56d89684b67d8b634be19e0034e6
Andres Campuzano Garzon <campuxano88@gmail.com>
2020-09-15 14:08:12 -0500

    code

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Container from "../styles/HomeStyles"
 const index = () => {
   return (
     <Container>
-      <h1>This is the home page...</h1>
+      <h1>This is the home page....</h1>
       <a href="https://andrescampuzano.com" target="_blank" rel="noreferrer">
         👉check andrescampuzano.com
       </a>
